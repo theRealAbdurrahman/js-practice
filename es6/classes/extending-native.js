1ff62d2dc87fa6d263f52c447483d70f173ae78f
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-06-22 18:48:10 +0200

    added the sort method to the class and compared between using for in and for of loop

--- es6/classes/extending-native.js
+++ es6/classes/extending-native.js
@@ -6,6 +6,9 @@ class MovieCollection extends Array {
     add(movie) {
         this.push(movie)
     }
+    topMovies(limit = 10) { // returns the movies sorted according to its stars. passing 2 as a limit param will return the top 2
+        return console.table(this.sort((a,b) => a.stars > b.stars ? 1 : -1).slice(0,limit));
+    }
 }
 
 const movies = new MovieCollection('Bedo\'s favs', 
@@ -21,7 +24,27 @@ const movies = new MovieCollection('Bedo\'s favs',
     name: "Into the Wild",
     stars: 9.7
 }, );
+
 movies.add({
     name: 'Mr. Nobody',
     stars: 9.7
 });
+
+
+
+// iterating on an class which is extended using for in loop 
+
+for ( const movie in movies) {
+    console.log(movie);
+    
+}
+// Output : 
+ // 1
+ // 2
+ // 3
+ // name >>> it prints everything
+
+ // using for of
+ for (const movie of movies) { // it behaves as we want 
+     console.table(movie);
+ }
