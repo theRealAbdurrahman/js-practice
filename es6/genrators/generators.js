d1d1bbe685a3c9582654b2b1de3d57d02fee46e4
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-06-24 14:30:35 +0200

    made an iterator to iterate over arrays using generator

--- es6/genrators/generators.js
+++ es6/genrators/generators.js
@@ -6,7 +6,7 @@ function* listPeople() {
 }
 // a function keyword followed by an asterisk is a generator.
 // a generator is basically a function that can stop midway and then continue from where it stopped.
-// it appears to be a function byt it behaves like an iterator that stops and continues when you call .next()
+// it appears to be a function but it behaves like an iterator that stops and continues when you call .next()
 
 const people = listPeople() // to use a generator you invoke the generator function and store it in a variable 
 
@@ -37,3 +37,51 @@ function* listNumbers() {
 }
 
 const numbers = listNumbers()
+
+// Another example: iterate over an array, but instead of logging its values we will use a generator and call .next()
+
+const inventorsList = [{ // I don't usually write this way but I'm using an auto formatter XD 
+        first: 'Albert',
+        last: 'Einstein',
+        year: 1879
+    },
+    {
+        first: 'Isaac',
+        last: 'Newton',
+        year: 1643
+    },
+    {
+        first: 'Galileo',
+        last: 'Galilei',
+        year: 1564
+    },
+    {
+        first: 'Marie',
+        last: 'Curie',
+        year: 1867
+    },
+    {
+        first: 'Johannes',
+        last: 'Kepler',
+        year: 1571
+    },
+    {
+        first: 'Nicolas',
+        last: 'Copernicus',
+        year: 1473
+    },
+    {
+        first: 'Max',
+        last: 'Planck',
+        year: 1858
+    },
+];
+
+function* iterate(list) {
+    for (const item of list) {
+        yield item;
+    }
+}
+
+const inventors = iterate(inventorsList)
+inventors.next().value
